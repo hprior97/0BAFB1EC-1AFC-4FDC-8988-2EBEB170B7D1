1c741a88088be6d9d33b880d177772d0b812ebff
Hamish Prior <109573172+hprior97@users.noreply.github.com>
2022-11-21 09:23:13 +0000

    Add logic to progress-service.ts

--- src/services/progress-service.ts
+++ src/services/progress-service.ts
@@ -6,5 +6,39 @@ export class ProgressWorker {
 	
 	constructor() { }
 	
+	generateReport(student: Student): void {
+		let reportData = this.getReportData(student.id);
+		
+		for(let assessmentID of Object.keys(reportData)) {
+			let responses = reportData[assessmentID] as StudentResponse[];
+			this.outputReportData(responses, responses.length, student);
+		}
+	}
 	
+	private getReportData(studentID: string): any {
+		const responseRepository = new ResponseRepository("responses");
+		
+		let completedResponses = responseRepository.findStudentResponsesWithAssessmentData(studentID);
+		
+		//Group the responses by their assessmentIDs
+		let group = completedResponses.reduce((r: any, a: any) => {
+			r[a.assessmentId] = [...r[a.assessmentId] || [], a];
+			return r;
+		}, {});
+		
+		return group;
+	}	
+	
+	private outputReportData(studentResponses: StudentResponse[], attempts: number, student: Student): void {
+		let studentLastName = student.lastName ? student.lastName : "lastname";
+		let studentFirstName = student.firstName ? student.firstName : "firstname";
+		var responseString = "\n" + studentFirstName + " " + studentLastName + " has completed " + studentResponses[0].assessment?.name + " " + attempts + " times in total. Date and raw score given below:\n\n";
+		var detailStrings: string[] = [];
+		for(let response of studentResponses) {
+			var responseStringDetail = "Date: " + response.completed + ", Raw Score: " + response.results.rawScore + " out of " + response.responses.length;
+			detailStrings.push(responseStringDetail);
+		}
+		let responseDetail = detailStrings.join("\n");
+		console.log(responseString + responseDetail + "\n");
+	}
 }
