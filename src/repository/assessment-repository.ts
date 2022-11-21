787d7eefa68a70f7e34c67a6908433700fa34aa3
Hamish Prior <109573172+hprior97@users.noreply.github.com>
2022-11-21 04:13:15 +0000

    Add assessment repo stub

---
+++ src/repository/assessment-repository.ts
@@ -0,0 +1,6 @@
+import { BaseRepository } from "./base/base-repository";
+import { Assessment } from "../models/Assessment";
+
+export class AssessmentRepository extends BaseRepository<Assessment> {
+	
+}
