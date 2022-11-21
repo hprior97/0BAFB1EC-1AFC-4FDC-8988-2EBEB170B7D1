c1129012f7b802790e516aea32326a5574dc2da4
Hamish Prior <109573172+hprior97@users.noreply.github.com>
2022-11-21 09:35:20 +0000

    Add imports to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,11 @@
 #!/usr/bin/env node
+import { ProgressService } from './services/progress-service';
+import { FeedbackService } from './services/feedback-service';
+import { Student } from './models/student';
+import { StudentRepository } from './repository/student-repository';
+import { AssessmentRepository } from './repository/assessment-repository';
+import readline from 'node:readline';
+import { stdin as input, stdout as output } from 'node:process';
 import chalk from 'chalk';
 const clear = require('clear');
 const figlet = require('figlet');
