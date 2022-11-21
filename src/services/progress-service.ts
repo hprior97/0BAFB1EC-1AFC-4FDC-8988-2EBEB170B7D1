e030c8a7c6fb5f47c6178d68ee4c7a151a0e12ce
Hamish Prior <109573172+hprior97@users.noreply.github.com>
2022-11-21 06:24:14 +0000

    Add progress service with class stub

---
+++ src/services/progress-service.ts
@@ -0,0 +1,10 @@
+import { StudentResponse } from "../models/student-response";
+import { Student } from '../models/student';
+import { ResponseRepository } from '../repository/response-repository';
+
+export class ProgressWorker {
+	
+	constructor() { }
+	
+	
+}
