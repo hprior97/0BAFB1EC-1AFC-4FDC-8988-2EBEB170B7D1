a107876e7a1e4d7f465f5cfed7dc40e1df9bd09a
Hamish Prior <109573172+hprior97@users.noreply.github.com>
2022-11-21 05:14:26 +0000

    Add question repo stub

---
+++ src/repository/question-repository.ts
@@ -0,0 +1,8 @@
+import { BaseRepository } from "./base/base-repository";
+import { Question } from "../models/Question";
+
+export class QuestionRepository extends BaseRepository<Question> {
+	
+	
+	
+}
