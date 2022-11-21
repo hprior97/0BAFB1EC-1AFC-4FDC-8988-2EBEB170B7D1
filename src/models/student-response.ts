cc11d97e0d31b142ade2f2e513528ddf6cc4582e
Hamish Prior <109573172+hprior97@users.noreply.github.com>
2022-11-21 10:05:23 +0000

    Add date object to student-response

--- src/models/student-response.ts
+++ src/models/student-response.ts
@@ -3,6 +3,7 @@ import { Assessment } from './assessment';
 import { ConfigOption } from './question';
 
 export class StudentResponse {
+	private completedDate: Date | undefined;
 	constructor(
 		public id: string,
 		public assessmentId: string,
@@ -23,6 +24,20 @@ export class StudentResponse {
 		this.results = results;
 		this.completed = completed;
 		this.assessment = assessment;
+		this.completedDate = this.assignCompletedDateObject(completed);
+	}
+	
+	assignCompletedDateObject(dateString?: string): Date | undefined {
+		if(dateString) {
+			const stringSplit = dateString.split(' ');
+			const dateStringSplit = stringSplit[0];
+			const timeStringSplit = stringSplit[1];
+			
+			const [day, month, year] = dateStringSplit.split('/');
+			const [hours, minutes, seconds] = timeStringSplit.split(':');
+			
+			return new Date(+year, +month - 1, +day, +hours, +minutes, + seconds);
+		}
 	}
 }
 
