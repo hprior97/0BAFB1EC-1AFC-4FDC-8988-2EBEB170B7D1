5c8029a2452f2468636e37562abbf4e680890259
Hamish Prior <109573172+hprior97@users.noreply.github.com>
2022-11-21 03:29:03 +0000

    Add cli title and confirm test rig working

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
 #!/usr/bin/env node
+import chalk from 'chalk';
+const clear = require('clear');
+const figlet = require('figlet');
+
+clear();
+console.log(
+	chalk.red(
+		figlet.textSync('ACER-Report', { horizontalLayout: 'half'})
+	)
+);
+
+export function add(value1: number, value2: number): number {
+	return value1 + value2;
+}
---
+++ src/spec/index.spec.ts
@@ -0,0 +1,8 @@
+import 'jasmine';
+import { add } from '..';
+
+describe("Index.ts Testing", () => {
+	it("should add numbers", () => {
+		expect(add(1, 2)).toBe(3);
+	})
+})
