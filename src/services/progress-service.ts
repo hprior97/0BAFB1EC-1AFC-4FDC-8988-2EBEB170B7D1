9ad99a0425cfee1312a9327fd5af3829807af3a0
Hamish Prior <109573172+hprior97@users.noreply.github.com>
2022-11-21 09:32:15 +0000

    Update name of progress-service export class

--- src/services/progress-service.ts
+++ src/services/progress-service.ts
@@ -2,7 +2,7 @@ import { StudentResponse } from "../models/student-response";
 import { Student } from '../models/student';
 import { ResponseRepository } from '../repository/response-repository';
 
-export class ProgressWorker {
+export class ProgressService {
 	
 	constructor() { }
 	
